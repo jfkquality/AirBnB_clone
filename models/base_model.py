4cabddae04c3d9fe1560af41326209a02b1bfc49
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-11-13 12:14:06 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,11 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            setattr(self, key, value)
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
 
     def __str__(self):
         """ string rep """
