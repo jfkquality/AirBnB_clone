b814bb281733a26c8db6fd8d765803ba3c367213
jfkquality <jfkquality@gmail.com>
2019-11-18 19:22:48 +0000

    AirBnB-clone, task 7. Update console.py

--- console.py
+++ console.py
@@ -1,9 +1,103 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 # Command interpreter for AirBnB project
-import cmd, sys
+import cmd, sys, gc
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    instances = []
+
+    def do_create(self, name):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        if not name:
+            print("** class name missing **")
+            return
+        try:
+            self.name = eval(name)()
+        except NameError:
+            print("** class doesn't exist **")
+            return
+        my_model = self.name
+        self.instances.append(my_model)
+        print(my_model.id)
+
+    def do_show(self, *args): # name=None, id=None):
+        """Prints the string representation of an instance based on the class name and id"""
+        params = ' '.join(args)
+        params = params.split(' ')
+
+        if len(params[0]) == 0:
+            print("** class name missing **")
+            return
+
+        if len(params) == 1:
+            print("** instance id missing **")
+            return
+
+        name = params[0]
+        id = params[1]
+
+
+        self.name = eval(name)()
+        # print("hi", self.name.__class__.__name__, "bye", BaseModel.__class__.__name__)
+
+        if self.name.__class__.__name__ != "BaseModel":
+        # try:
+        #     self.name = eval(name)()
+        # except NameError:
+            print("** class doesn't exist **")
+            return
+
+        self.id = id
+
+        # print("hello", self.name.__class__.__name__)
+        # print("goodbye", vars(self.name))
+
+        for obj in gc.get_objects():
+            if isinstance(obj, BaseModel):
+                print(obj)
+
+        for inst in self.instances:
+            if inst.id == self.id:
+                print(self.name)
+                return
+            else:
+                print("** no instance found **")
+                return
+
+        # try:
+        #     self.id = my_model.id
+        # except NameError:
+        #     print("** no instance found **")
+        #     return
+
+
+    def do_destroy(self, name, id):
+        """Deletes an instance based on the class name and id (save the change into the JSON file)"""
+        if not name:
+            print("** class name missing **")
+        if not id:
+            print("** instance id missing **")
+        if not attr_name:
+            print("** attribute name missing **")
+        if not attr_val:
+            print("** value missing **")
+
+    def do_all(self, name=None):
+        """Prints all string representation of all instances based or not on the class name"""
+
+        for obj in gc.get_objects():
+            if isinstance(obj, BaseModel):
+                print(obj)
+
+
+    def do_update(self, name, id, attr_name, attr_val):
+        """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)."""
+        if not name:
+            print("** class name missing **")
+        if not id:
+            print("** instance id missing **")
+
 
     def do_quit(self, arg):
         """ Quit command to exit the program """
